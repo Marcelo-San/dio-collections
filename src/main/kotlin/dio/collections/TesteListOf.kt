68f2324aaf64132315a9fe7d2306d374699764ef
Marcelo-San <marcelosantos.empreendedor@gmail.com>
2021-04-26 17:22:27 -0300

    Organizando grupos com o groupBy.

--- src/main/kotlin/dio/collections/TesteListOf.kt
+++ src/main/kotlin/dio/collections/TesteListOf.kt
@@ -2,9 +2,9 @@ package dio.collections
 
 fun main() {
     //Criando os objetos.
-    val marcelo = Funcionario("Marcelo", 2000.0)
-    val nelson = Funcionario("Nelson", 1500.0)
-    val alex = Funcionario("Alex", 4000.0)
+    val marcelo = Funcionario("Marcelo", 2000.0, "PJ")
+    val nelson = Funcionario("Nelson", 1500.0, "CLT")
+    val alex = Funcionario("Alex", 4000.0, "CLT")
 
     val funcionarios = listOf(marcelo, nelson, alex)
 
@@ -18,12 +18,20 @@ fun main() {
     //Ordenando a lista de funcionários através do salário.
     println("----------------------")
     funcionarios.sortedBy { it.salario }.forEach { println(it) }//Aqui, estamos passando como queremos ordenar.
+
+    //Agrupando a lista de funcionários por categoria CLT.
+    println("----------------------")
+    funcionarios
+        //Aqui, estamos passando como queremos agrupar (tipoContratacao). A nossa categoria vai virar uma "chave".
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
 }
 
 //Criando uma classe de dados para os objetos.
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
